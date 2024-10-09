193d200b379e21b31a9be6a13f87bf9931bd37ad
anwarroman07 <anwarroman07@gmail.com>
2024-10-09 11:12:40 +0530

    js file added

---
+++ A.js
@@ -0,0 +1,23 @@
+var Season="summer"
+if(Season=="spring"){
+
+    console.log("Enjoy the blooming flowers");
+    
+}
+if(Season=="summer"){
+console.log("Have fun in the summer");
+
+
+}
+if(Season=="autumn"){
+    console.log("Admire the colorfuk leaves");
+    
+}
+if(Season=="winter"){
+console.log("Bundle up and stay warm");
+
+}
+else{
+    console.log("Please did your HW");
+    
+}
